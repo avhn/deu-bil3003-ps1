e9c766be57639fa678441ae6c767009afaacd6f2
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-10-20 03:52:52 +0300

    Add parse module as completed

---
+++ assignment/parse.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+def normalize(value):
+
+    """Convert a String object to floating or decimal if possible."""
+    
+    try:
+        if value.isdigit():
+            result = int(value)
+        else:
+            result = float(value)
+        return result
+
+    except (ValueError, AttributeError):
+        return value
+
+
+def parse_dataset(filename='dataset.csv'):
+
+    """Parse .csv file.
+
+    Args:
+        filename: 
+
+    Returns:
+
+    """
+
+    dataset = list()
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip().split(',')
+            if len(dataset) == 0:
+                dataset = [[item] for item in line]
+            else:
+                for i, item in enumerate(line):
+                    if item == '?':
+                        item = None
+                    dataset[i].append(normalize(item))
+    return dataset
