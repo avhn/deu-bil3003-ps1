87460d154a2c42692e6f07f2c0c973621f3c4e57
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-10-22 16:11:47 +0300

    Add tests for parse module

---
+++ test.py
@@ -0,0 +1,25 @@
+import unittest
+from assignment import parse
+
+
+class ParseTests(unittest.TestCase):
+
+    decimalString = '12312312'
+    floatString = '12312.323210'
+    sampleString = 'bla bla'
+    
+    def test_normalize(self):
+        assert int == type(parse.normalize(self.decimalString))
+        assert float == type(parse.normalize(self.floatString))
+        assert str == type(parse.normalize(self.sampleString))
+
+    def test_parse_dataset(self):
+        dataset = parse.parse_dataset()
+        # may do more extensive test later
+        if not dataset or len(dataset[0]) < 2:
+            raise ValueError('Dataset is not parsed properly.')
+
+
+if __name__ == '__main__':
+    
+    unittest.main()
