6db0dbfe2907c68eebc48ba4d3bceae3a011e7d9
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-10-30 17:31:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from problemset import interact
+from problemset import parse, gen_itemset
+
+
+def dev_test():
+    indicators, dataset = parse.parse_dataset(None)
+    gen_itemset.gen_freq_itemsets(None, dataset)
 
 
 def main():
-    support_value, metric, leverage_value = interact.get_input()    
+    support_value, metric, leverage_value, filepath = interact.get_input()
 
-    
-if __name__ = '__main__':
-    main()
+    indicators, dataset = parse.parse_dataset()
+    gen_itemset.gen_freq_itemsets(None, dataset)
+
+
+if __name__ == '__main__':
+    dev_test()
