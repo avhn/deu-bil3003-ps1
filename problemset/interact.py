b3b31235354e8fba1f80ef4bc388f1f4adcaa5ab
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-10-30 17:30:48 +0300

    Update interact
    Clean up

--- problemset/interact.py
+++ problemset/interact.py
@@ -3,23 +3,21 @@
 import os
 from .metric_options import Metric
 
-METRIC_OPTIONS = ['confidence', 'lift', 'leverage']
-
 
 def get_input():
-
     """
     Get corresponding inputs from user.
 
     Returns:
         A tuple such as:
-            (minimum support value, metric, leverage value)
+            (minimum support value, metric, leverage value, filepath to parse)
     """
-    
+
     try:
         minimum_support = float(input('Minimum support value:\t'))
 
-        metric_option = int(input('Choose metric. Confidence[1] / Lift[2] / Leverage[3]:\t'))
+        metric_option = int(
+            input('Choose metric. Confidence[1] / Lift[2] / Leverage[3]:\t'))
         for m in Metric:
             if metric_option == m.value:
                 metric_option = m
@@ -31,7 +29,10 @@ def get_input():
 
         min_threshold = float(input('Minimum threshold:\t'))
 
-        return minimum_support, metric_option, min_threshold
+        filepath = input(
+            'Absolute filepath which contains comma seperated values[Enter for default]:\t')
+
+        return minimum_support, metric_option, min_threshold, filepath
 
     except (ValueError, TypeError) as e:
         print("{}Invalid input:{}\t{}"
@@ -48,4 +49,3 @@ def get_input():
     finally:
         print("Restarting input sequence." + os.linesep)
         return get_input()
-
