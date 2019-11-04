e86fc3ae63b648829abe589c05409c5c72708f1d
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-11-04 06:28:05 +0300

    Update problemset.interact and main

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 # -*- coding: utf-8 -*-
 
 from problemset import interact
-from problemset import parse, generate
+from problemset import parse
+from problemset import generate
+from problemset.utils import Metric
 
 
 def dev_test():
     dataset = parse.parse_dataset()
-    frequent_itemsets = generate.frequent_itemsets(0.05, dataset)
+    frequent_itemsets = generate.frequent_itemsets(0.1, dataset)
     for i, l in enumerate(frequent_itemsets):
         print("LEVEL {}".format(i + 1))
         for itemset in l:
-            print(itemset, l[itemset] * len(dataset))
+            print(itemset)
+    
+    association_rules = generate.association_rules(dataset, frequent_itemsets, Metric.Confidence, 0.97)
+    for i in association_rules:
+        print(i[0], '->', i[1])
 
 
 def main():
-    support_value, metric, leverage_value, filepath = interact.get_input()
-
-    indicators, dataset = parse.parse_dataset(filepath)
-    generate.frequent_itemsets(None, dataset)
+    LOG_FILE = 'out.txt'
+    support_threshold, metric, metric_threshold, filepath = interact.get_input()
+    dataset = parse.parse_dataset(filepath)
+    frequent_itemsets = generate.frequent_itemsets(support_threshold, dataset)
+    interact.print_frequent_itemsets(frequent_itemsets, LOG_FILE)
+    association_rules = generate.association_rules(dataset, frequent_itemsets, metric, metric_threshold)
+    interact.print_association_rules(association_rules, LOG_FILE, metric)
 
 
 if __name__ == '__main__':
-    dev_test()
+    main()
--- problemset/interact.py
+++ problemset/interact.py
@@ -1,8 +1,27 @@
 # -*- coding: utf-8 -*-
 
 import os
-from .metrics import Metric
+from itertools import chain
 
+from .utils import Metric
+from .utils import print_and_write
+from .utils import format_item
+from .utils import round_2
+
+INDENT = ' ' * 4
+LINE = '-' * 50
+
+name_switch = {
+    Metric.Confidence: 'Confidence',
+    Metric.Leverage: 'Leverage',
+    Metric.Lift: 'Lift'
+}
+
+validation_switch = {
+    Metric.Confidence: lambda x: 0 <= x <= 1,
+    Metric.Leverage: lambda x: -0.25 <= x <= 0.25,
+    Metric.Lift: lambda x: 0 <= x
+}
 
 def get_input():
     """
@@ -10,29 +29,33 @@ def get_input():
 
     Returns:
         A tuple such as:
-            (minimum support value, metric, leverage value, filepath to parse)
+            (minimum support value, metric, metric value, filepath to parse)
     """
-
+    
+    print(LINE + os.linesep)
     try:
-        minimum_support = float(input('Minimum support value:\t'))
+        minsup = float(input('Minimum support value:\t'))
+        if not 0 <= minsup <= 1:
+            raise ValueError("Invalid support value: " + minsup)
 
-        metric_option = int(
+        metric = int(
             input('Choose metric. Confidence[1] / Lift[2] / Leverage[3]:\t'))
         for m in Metric:
-            if metric_option == m.value:
-                metric_option = m
+            if metric == m.value:
+                metric = m
                 break
-
-        if metric_option not in Metric:
+        if metric not in Metric:
             raise ValueError("Invalid metric option -> {}"
-                             .format(metric_option))
+                             .format(metric))
 
-        min_threshold = float(input('Minimum metric threshold:\t'))
+        minsup_threshold = float(input('Minimum metric threshold:\t'))
+        if not validation_switch[metric](minsup_threshold):
+            raise ValueError("Invalid {} value: {}".format(name_switch[metric], minsup_threshold))
 
         filepath = input(
-            'Absolute filepath which contains comma seperated values[Enter for default]:\t')
+            'Absolute filepath which contains comma seperated values [Enter for default]:\t')
 
-        return minimum_support, metric_option, min_threshold, filepath
+        return minsup, metric, minsup_threshold, filepath
 
     except (ValueError, TypeError) as e:
         print("{}Invalid input:{}\t{}"
@@ -45,7 +68,50 @@ def get_input():
     except BaseException as e:
         print("{}Encountered unexpected exception: {}"
               .format(os.linesep, e))
+    
+    print("Restarting input sequence." + os.linesep)
+    return get_input()
 
-    finally:
-        print("Restarting input sequence." + os.linesep)
-        return get_input()
+
+def print_frequent_itemsets(frequent_itemsets: list, filepath: str):
+    """
+    Args:
+        frequent_itemset: Output of .generate.frequent_itemsets
+        filepath: Name of a file or absolute filepath
+    """
+    
+    with open(filepath, 'a+') as file:
+        print_and_write(os.linesep + LINE, file)
+        print_and_write("{}Frequent item sets:{}".format(*(os.linesep * 2)), file)
+        for itemset in chain.from_iterable(frequent_itemsets):
+            string = INDENT + '{ '
+            for item in itemset:
+                string += format_item(item) + ", "
+            string = string[:-2] + " }"
+            print_and_write(string, file)
+
+
+def print_association_rules(association_rules: list, filepath: str, metric: Metric):
+    """
+    Args:
+        association_rules: Output of .generate.association_rules
+        filepath: Name of a file or absolute filepath
+        metric: Metric enum
+    """
+    
+    with open(filepath, 'a+') as file:
+        print_and_write(os.linesep + LINE, file)
+        print_and_write("{}Association rules:{}".format(*(os.linesep * 2)), file)
+        for i, rule in enumerate(association_rules):
+            rule, support, metric_value  = rule
+
+            string = INDENT + '{}. IF '.format(i + 1)
+            for item in rule[0]: # antecedent
+                string += format_item(item) + ' AND '
+            string = string[:-4] + "THEN "
+            for item in rule[1]: # consequent
+                string += format_item(item) + ' AND '
+            string = string[:-4] + os.linesep + INDENT \
+                    + '-> with Support: {} and {}: {}' .format(round_2(support), name_switch[metric], round_2(metric_value)) \
+                    + os.linesep
+            print_and_write(string, file)
